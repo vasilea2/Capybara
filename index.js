ea15daaef56defb34a00c3c7e0b021695c49870f
Andrei Vasile <andreisvasile@gmail.com>
2022-07-08 05:14:18 +0300

    split w2g and cpbr into files with added functionality (list, -n and help capybara)
    Signed-off-by: Andrei Vasile <andreisvasile@gmail.com>

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 // 1. w2g -> create w2g link
 
 require("dotenv").config();
-const fetch = require('node-fetch');
-const capybaras = require('./CapybaraItems').CapybaraFiles
 
 const { Client, Intents } = require('discord.js');
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const w2g = require('./W2g')
+const capybara = require('./Capybara')
 
 //Listen to the event that signals the bot is ready to start working
 client.on("ready", () => {
@@ -20,20 +20,18 @@ client.on("messageCreate", async (message) => {
 
   // Generate a w2g link when typed w2g
   if (message.content.toLowerCase() === "w2g") {
-    createW2gChannel.then(link => { message.reply(link) })
+    w2g.GetLink.then(link => message.reply(link))
   }
 
-  if (message.content.toLowerCase() === "capybara" || message.content.toLowerCase() === "cpbr") {
-    let luckyNumber = between(0, capybaras.size)
-    let myCapybara = capybaras.get(luckyNumber)
-    message.reply("Azi esti o " + myCapybara.name.split(".")[0])
-    message.reply({
-      files: [{
-        attachment: myCapybara.url,
-        name: myCapybara.name
-      }]});
+  // Generate capybara
+  if (message.content.toLowerCase().includes("capybara") || message.content.toLowerCase().includes("cpbr")) {
+    capybara.HandleCapybara(message).then(result => {
+      if (result[0]) message.reply(result[0])
+      if (result[1]) message.reply(result[1])
+    })
   }
 
+  // Supplimentary additions
   if (message.content === "Cine e smecher?") {
     if (message.member.user.username === "Pojo") message.reply("Cine intreaba e smecher")
     else message.reply(`In nici un caz ${message.member.user.username}`)
@@ -42,29 +40,3 @@ client.on("messageCreate", async (message) => {
 
 //Login to the server using bot token
 client.login(process.env.DISCORD_TOKEN);
-
-const createW2gChannel = new Promise((resolve) => {
-  fetch("https://w2g.tv/rooms/create.json", {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-          "w2g_api_key": process.env.W2G_API_KEY,
-          "share": "https://www.youtube.com/watch?v=m1k3Cpke4yU",
-          "bg_color": "#000000",
-          "bg_opacity": "50"
-      })
-  })
-  .then(response => response.json())
-  .then(function (data) {
-      resolve("https://w2g.tv/rooms/" + data.streamkey)
-  });
-})
-
-function between(min, max) {  
-  return Math.floor(
-    Math.random() * (max - min) + min
-  )
-}
